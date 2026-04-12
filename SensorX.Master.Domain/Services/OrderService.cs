40071e197b61d7c7ec87236e40293eff880d9b5a
=cdhai16 <chuduchai1901@gmail.com>
2026-04-12 20:44:18 +0700

    feat(domain): add order service

---
+++ SensorX.Master.Domain/Services/OrderService.cs
@@ -0,0 +1,25 @@
+using SensorX.Master.Domain.Contexts.OrderContext.AggregateModels.InvoiceAggregate;
+using SensorX.Master.Domain.Contexts.OrderContext.AggregateModels.OrderAggregate;
+
+namespace SensorX.Master.Domain.Services;
+
+public class OrderService
+{
+    public Invoice CreateInvoiceFromOrder(Order order)
+    {
+        var subTotal = order.GetSubtotal();
+        var taxAmount = order.GetTotalTax();
+        var invoice = Invoice.Create(order.Id, order.CustomerInfo, subTotal, taxAmount);
+        foreach (var item in order.Items)
+        {
+            invoice.AddItem(InvoiceItem.Create(item.Id, item.ProductId, item.ProductName, item.Unit, item.Quantity, item.UnitPrice, item.TaxRate));
+        }
+        return invoice;
+    }
+
+    public void CancelOrderByCustomer(Order order, Invoice invoice)
+    {
+        order.Status = OrderStatus.Cancelled;
+        invoice.Status = InvoiceStatus.Cancelled;
+    }
+}
