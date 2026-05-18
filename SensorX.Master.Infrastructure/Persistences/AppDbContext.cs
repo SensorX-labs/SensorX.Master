d51695825b559b959be91e117d92dec790886f38
tungsk <skfreelancer2004@gmail.com>
2026-05-19 01:29:09 +0700

    fix: AppdbContext remove dbSet don't use

--- SensorX.Master.Infrastructure/Persistences/AppDbContext.cs
+++ SensorX.Master.Infrastructure/Persistences/AppDbContext.cs
@@ -3,25 +3,21 @@ using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SensorX.Master.Application.Common.DomainEvent;
-using SensorX.Master.Domain.SeedWork;
-using SensorX.Master.Domain.Contexts.OrderContext.AggregateModels.OrderAggregate;
 using SensorX.Master.Domain.Contexts.OrderContext.AggregateModels.InvoiceAggregate;
-using SensorX.Master.Domain.Contexts.SupplyChainContext.AggregateModels.WarehouseAggregate;
-using SensorX.Master.Domain.Contexts.SupplyChainContext.ReadModels;
+using SensorX.Master.Domain.Contexts.OrderContext.AggregateModels.OrderAggregate;
 using SensorX.Master.Domain.Contexts.SupplyChainContext.AggregateModels.SupplyRequestAggregate;
 using SensorX.Master.Domain.Contexts.SupplyChainContext.AggregateModels.TransferOrderAggregate;
+using SensorX.Master.Domain.Contexts.SupplyChainContext.AggregateModels.WarehouseAggregate;
+using SensorX.Master.Domain.Contexts.SupplyChainContext.ReadModels;
+using SensorX.Master.Domain.SeedWork;
 
 namespace SensorX.Master.Infrastructure.Persistences;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<DomainEventOutbox> DomainEventOutboxes { get; set; }
-    public DbSet<Order> Orders => Set<Order>();
-    public DbSet<Invoice> Invoices => Set<Invoice>();
     public DbSet<SensorX.Master.Domain.Contexts.SupplyChainContext.AggregateModels.WarehouseAggregate.Warehouse> Warehouses => Set<SensorX.Master.Domain.Contexts.SupplyChainContext.AggregateModels.WarehouseAggregate.Warehouse>();
     public DbSet<WarehouseInventoryProjection> WarehouseInventoryProjections => Set<WarehouseInventoryProjection>();
-    public DbSet<SupplyRequest> SupplyRequests => Set<SupplyRequest>();
-    public DbSet<TransferOrder> TransferOrders => Set<TransferOrder>();
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
