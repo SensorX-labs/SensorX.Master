02303b9168414ea9cc69333708d2a8044ccabaad
=cdhai16 <chuduchai1901@gmail.com>
2026-06-15 09:29:38 +0700

    fix : sửa lỗi chỉ cho lọc theo mã đơn hàng

--- SensorX.Master.Application/Common/QueryExtensions/Search/OrderSearch.cs
+++ SensorX.Master.Application/Common/QueryExtensions/Search/OrderSearch.cs
@@ -13,13 +13,6 @@ public static class OrderSearch
 
         var term = searchTerm.Trim().ToLower();
 
-        return query.Where(o =>
-            o.Code.Value.ToLower().Contains(term) ||
-            (o.DeliveryInfo.CompanyName != null && o.DeliveryInfo.CompanyName.ToLower().Contains(term)) ||
-            (o.DeliveryInfo.RecipientName != null && o.DeliveryInfo.RecipientName.ToLower().Contains(term)) ||
-            (o.DeliveryInfo.RecipientPhone != null && o.DeliveryInfo.RecipientPhone.Value.ToLower().Contains(term)) ||
-            (o.DeliveryInfo.Email != null && o.DeliveryInfo.Email.Value.ToLower().Contains(term)) ||
-            (o.DeliveryInfo.ShippingAddress != null && o.DeliveryInfo.ShippingAddress.ToLower().Contains(term)) ||
-            (o.SenderInfo.Name != null && o.SenderInfo.Name.ToLower().Contains(term)));
+        return query.Where(o => o.Code.Value.ToLower().Contains(term));
     }
 }
